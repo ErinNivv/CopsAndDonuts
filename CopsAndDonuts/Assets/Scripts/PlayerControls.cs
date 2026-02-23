a42099fa597f34d5c11a460fd8ec2c05408f4976
erinnivv <erin.govender09@gmail.com>
2026-02-23 22:48:29 +0200

    Player Controls
    Player 1 - not moving

---
+++ CopsAndDonuts/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("PLAYER")]
+    private Vector2 moveP1;
+    private float moveSpeed = 5f;
+    private Rigidbody2D rbP1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rbP1 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbP1.linearVelocity = new Vector2(moveP1.x,moveP1.y) * moveSpeed * Time.deltaTime;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveP1 = context.ReadValue<Vector2>();
+    }
+}
