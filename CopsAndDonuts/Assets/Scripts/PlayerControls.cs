e94a261db4421aa8e7e0c0ab8e386fa7988e26b1
liliyy-t <queenlilithatanou@gmail.com>
2026-02-25 17:10:38 +0200

    player raycast

--- CopsAndDonuts/Assets/Scripts/PlayerControls.cs
+++ CopsAndDonuts/Assets/Scripts/PlayerControls.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -9,77 +10,91 @@ public class PlayerControls : MonoBehaviour
     private float grabRange = 2.5f;
 
     private Vector2 moveP1;
-    private Vector2 moveP2;
-    private Vector2 moveP3;
 
     private float interactP1;
-    private float interactP2;
-    private float interactP3;
 
     [SerializeField] Transform rayP1;
 
     [SerializeField] private Rigidbody2D rbP1;
-    [SerializeField] private Rigidbody2D rbP2;
-    [SerializeField] private Rigidbody2D rbP3;
 
+    [Header("Door")]
+    [SerializeField] public GameObject door;
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private List<Sprite> Sprites;
+
+    //input Manager
+    private PlayerInput playerInput;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rbP1 = GetComponent<Rigidbody2D>();
-        rbP2 = GetComponent<Rigidbody2D>();
-        rbP3 = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+
+        // if its Player 1
+        if (playerInput.playerIndex == 0)
+        {
+            //Assign Sprites
+        }
+        else if (playerInput.playerIndex == 1)//if its Player 2
+        {
+            //Assign Sprites
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         rbP1.linearVelocity = new Vector2(moveP1.x * moveSpeed, moveP1.y * moveSpeed);
-        rbP2.linearVelocity = new Vector2(moveP2.x * moveSpeed, moveP2.y * moveSpeed);
-        rbP3.linearVelocity = new Vector2(moveP3.x * moveSpeed, moveP3.y * moveSpeed);
 
-        //if (interactP1 > 1)
-        //{
-        //    Ray ray = new Ray (rayP1.position,rayP1.forward);
-        //    RaycastHit2D hit;
+      
 
-        //    Debug.DrawRay(rayP1.position, rayP1.forward * grabRange, Color.red, 2f);
 
-        //    if (Physics.Raycast(ray, out hit, grabRange))
-        //    {
-        //        if (hit.collider.CompareTag("Donut"))
-        //        {
-        //            print("picked up");
-        //        }
-        //    }
-        //}
-
-        
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         moveP1 = context.ReadValue<Vector2>();
-        moveP2 = context.ReadValue<Vector2>();
-        moveP3 = context.ReadValue<Vector2>();
     }
 
     public void Interact(InputAction.CallbackContext context)
     {
-        interactP1 = context.ReadValue<float>();
-        interactP2 = context.ReadValue<float>();
-        interactP3 = context.ReadValue<float>();
+        //interactP1 = context.ReadValue<float>();
+        //interactP2 = context.ReadValue<float>();
+        //interactP3 = context.ReadValue<float>();
+
+        if (context.performed)
+        {
+            
+            RaycastHit2D hit = Physics2D.Raycast(rayP1.position, rayP1.up, 3);
+                if (hit.collider != null)
+                {
+                if (hit.collider.CompareTag("Donut"))
+                {
+                    print("Working");
+                }
+                }
+            Debug.DrawRay(rayP1.position, rayP1.up, Color.red, 3f);
+        }
+        
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (interactP1 > 0)
+    //    {
+    //        if (other.CompareTag("Donut"))
+    //        {
+    //            print("pick up");
+    //        }
+    //    }
+
+    //}
+
+    public void Door()
     {
-        if (interactP1 > 0)
-        {
-            if (other.CompareTag("Donut"))
-            {
-                print("pick up");
-            }
-        }
-            
+        
     }
 }
