81418423a019401f8842d76613d6dea69e362b2f
liliyy-t <queenlilithatanou@gmail.com>
2026-02-25 23:22:32 +0200

    drag and drop working

--- CopsAndDonuts/Assets/Scripts/PlayerControls.cs
+++ CopsAndDonuts/Assets/Scripts/PlayerControls.cs
@@ -15,6 +15,10 @@ public class PlayerControls : MonoBehaviour
 
     [SerializeField] Transform rayP1;
 
+    [Header("PickUp")]
+    [SerializeField] Transform holdPoint;
+    private GameObject heldObject;
+
     [SerializeField] private Rigidbody2D rbP1;
 
     [Header("Door")]
@@ -24,7 +28,9 @@ public class PlayerControls : MonoBehaviour
 
     //input Manager
     private PlayerInput playerInput;
-    
+
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -50,8 +56,6 @@ public class PlayerControls : MonoBehaviour
     {
         rbP1.linearVelocity = new Vector2(moveP1.x * moveSpeed, moveP1.y * moveSpeed);
 
-      
-
 
     }
 
@@ -62,51 +66,82 @@ public class PlayerControls : MonoBehaviour
 
     public void Interact(InputAction.CallbackContext context)
     {
-        
-
         if (context.performed)
         {
+            // Check if already holding something
+            if (heldObject != null)
+            {
+                // DROP the donut
+                print("Dropping donut!");
+                DropObject();
+            }
+            else
+            {
+                // PICK UP a donut
+                Vector2 direction = transform.right;
+                int layerMask = LayerMask.GetMask("Donut");
 
-            Vector2 direction = transform.right;
-            int layerMask = LayerMask.GetMask("Donut");
-
-            RaycastHit2D hit = Physics2D.Raycast(rayP1.position, direction, grabRange, layerMask);
-            Debug.DrawRay(rayP1.position, direction * grabRange, Color.red, 0.5f);
+                RaycastHit2D hit = Physics2D.Raycast(rayP1.position, direction, grabRange, layerMask);
+                Debug.DrawRay(rayP1.position, direction * grabRange, Color.red, 0.5f);
 
                 if (hit.collider != null)
                 {
-                print("Working Donut"+ hit.collider.name);
-                //PickUpObject(hit.collider.gameObject);
-                
+                    print("Working Donut: " + hit.collider.name);
+                    PickUpObject(hit.collider.gameObject);
                 }
-
-           
+                else
+                {
+                    print("No donut found to pick up");
+                }
+            }
         }
-        
     }
 
-    //void PickUpObject(GameObject obj)
-    //{
-    //    heldObject = obj;
+    void PickUpObject(GameObject obj)
+    {
+        heldObject = obj;
 
-    //    // Make it a child of the hold point
-    //    obj.transform.SetParent(holdPoint);
+        // Make it a child of the hold point
+        obj.transform.SetParent(holdPoint);
 
-    //    // Reset position to match hold point
-    //    obj.transform.localPosition = Vector3.zero;
+        // Reset position to match hold point
+        obj.transform.localPosition = Vector3.zero;
 
-    //    // Disable physics so it doesn't interfere while held
-    //    if (obj.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
-    //    {
-    //        rb.simulated = false;
-    //    }
-    //    if (obj.TryGetComponent<Collider2D>(out Collider2D col))
-    //    {
-    //        col.enabled = false;
-    //    }
-    //}
+        // Disable physics so it doesn't interfere while held
+        if (obj.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.simulated = false;
+        }
+        if (obj.TryGetComponent<Collider2D>(out Collider2D col))
+        {
+            col.enabled = false;
+        }
 
-    
+        print("Donut is picked");
+    }
 
+    void DropObject()
+    {
+
+        if (heldObject == null) return;
+
+        // Turm the physics back on 
+        Rigidbody2D rb = heldObject.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.simulated = true;
+        }
+
+        Collider2D col = heldObject.GetComponent<Collider2D>();
+        if (col != null)
+        {
+            col.enabled = true;
+        }
+
+        heldObject.transform.SetParent(null);
+        print("Donut is Dropped");
+        heldObject = null;
+
+    }
 
 }
