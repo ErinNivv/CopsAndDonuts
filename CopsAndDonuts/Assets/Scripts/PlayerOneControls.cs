41bae9ad982a38a2c66eb01f5c947f2e8c9b0bb4
erinnivv <erin.govender09@gmail.com>
2026-02-25 10:55:49 +0200

    Changed back commit

--- CopsAndDonuts/Assets/Scripts/PlayerOneControls.cs
+++ CopsAndDonuts/Assets/Scripts/PlayerOneControls.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerOneControls : MonoBehaviour
+public class PayerOneControls : MonoBehaviour
 {
     [Header("PLAYER3")]
     private float moveSpeed = 5f;
